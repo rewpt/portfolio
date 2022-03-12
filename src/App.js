8a771d9d53ae800e0dedd0ac8a9be9a61c096c6d
rewpt <zpermack@gmail.com>
2022-03-11 20:35:16 -0500

    clean up unused files and add images and components for file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+
     </div>
   );
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="flex flex-between">
+        <li> Home</li>
+        <li>linked in</li>
+        <li>Github</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
