0846a0ac2cc9c7e383ff4605ad87d00f8be71cb1
rewpt <zpermack@gmail.com>
2022-03-15 15:50:34 -0400

    add logo cutout for profile and styling for top banner

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import logo from "../../images/logo.svg";
+import Logo from "../Logo";
+
 function Nav() {
   return (
     <nav className="bg-slate-200 sticky top-0 px-5 py-3">
       <ul className="flex justify-between">
-        <div className="flex left-buttons">
+        <div className="flex left-buttons ml-[20px]">
+          <Logo />
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className="flex right-buttons">
-          <li>Projects</li>
-          <li>Contact</li>
-          <li>Github</li>
+          <li className="text-xl mr-[20px]">Projects</li>
+          <li className="text-xl mr-[20px]">Contact</li>
+          <li className="text-xl mr-[10px]">Github</li>
         </div>
       </ul>
     </nav>
--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "./Button";
-import face from "../images/face.png";
+import face from "../images/cutout2.png";
 export default function TopBanner() {
   return (
-    <div className="h-[90vh] text-5xl flex flex-col justify-center items-center align-center bg-slate-200">
-      <div className="flex justify-center items-center ring-6 h-[500px] w-[500px] ring-white p-10 bg-slate-400 rounded-full">
-        <img src={face} className="face" alt="logo" />
+    <div className="h-[70vh] text-5xl flex flex-col justify-center items-center bg-slate-200">
+      <div className="flex overflow-hidden justify-center items-start rounded-full h-[550px] w-[550px] mx-[100px] ring-white p-10 bg-slate-400 ">
+        <img src={face} className="face h-[525px] max-w-none" alt="profile" />
       </div>
-      <p className="mb-5 mt-10">
+      <p className="w-[900px] mb-5 mt-10 mx-[100px]">
         Hi! My name is
         <span className="text-[orange]"> Zev Permack. </span> I build web apps
         to help improve the lives of others!
