cd8245754d825bb13e83b06d16210ac41e354d26
rewpt <zpermack@gmail.com>
2022-03-19 18:02:45 -0400

    add base url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 import ProjectCarousel from './components/ProjectCarousel';
 
 render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes >
       <Route path="/" element={<App />}>
         <Route path="/projects" element={<ProjectCarousel />}>
