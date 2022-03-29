02b9f2500713c30166fee2aadab87069003bec89
rewpt <zpermack@gmail.com>
2022-03-29 19:18:55 -0400

    change animate to opacity and modularize class names

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -6,87 +6,102 @@ import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
 import openInNewTab from "./helpers/openNewTab";
 import LogoStatic from "./LogoStatic";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+
+const modalVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.5 },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
 
 export default function NavModal(props) {
   const { navOpen, setNavOpen, chevronClass, svgVariants } = props;
   return (
     <>
-      {navOpen && (
-        <ul className="absolute w-[300px] py-[1em] px-[.5em] h-[100vh ] top-0 z-20 bg-gradient-to-r from-zpurple to-zlpurple flex flex-col drop-shadow-xl">
-          <div className="flex">
-            <div
-              className="h-[30vw] w-[25vw] max-h-[138px] max-w-[120px]"
+      <AnimatePresence>
+        {navOpen && (
+          <motion.ul
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            className="absolute max-w-[300px] py-[1em] px-[1em] h-[100vh ] top-0 right-0 left-0 z-20 bg-gradient-to-r from-zpurple to-zlpurple flex flex-col drop-shadow-xl"
+          >
+            <div className="flex">
+              <div
+                className="h-[30vw] w-[25vw] max-h-[138px] max-w-[120px]"
+                onClick={() => {
+                  setNavOpen(false);
+                }}
+              >
+                <LogoStatic />
+              </div>
+              <div
+                className={chevronClass}
+                onClick={() => {
+                  setNavOpen(!navOpen);
+                }}
+              >
+                <FontAwesomeIcon
+                  className="h-[15vw] w-[15vw] max-h-[60px] max-w-[60px] stroke-2 stroke-black "
+                  icon={faAngleUp}
+                />
+              </div>
+            </div>
+            <a
+              href="#projects"
               onClick={() => {
                 setNavOpen(false);
               }}
             >
-              <LogoStatic />
-            </div>
-            <div
-              className={chevronClass}
+              <li className="modal-item-container">
+                <FontAwesomeIcon className="modal-icon" icon={faFolder} />
+                <span class="modal-text">Projects</span>
+              </li>
+            </a>
+            <a
               onClick={() => {
-                setNavOpen(!navOpen);
+                setNavOpen(false);
+              }}
+              href="#contact"
+            >
+              <li className="modal-item-container">
+                <FontAwesomeIcon className="modal-icon" icon={faEnvelope} />
+                <span class="modal-text">Contact</span>
+              </li>
+            </a>
+            <div
+              onClick={() => {
+                openInNewTab("https://www.github.com/rewpt");
+                setNavOpen(false);
               }}
             >
-              <FontAwesomeIcon
-                className="h-[15vw] w-[15vw] max-h-[60px] max-w-[60px] stroke-2 stroke-black"
-                icon={faAngleUp}
-              />
+              <li className="modal-item-container">
+                <FontAwesomeIcon className="modal-icon" icon={faGithub} />
+                <span class="modal-text">Github</span>
+              </li>
             </div>
-          </div>
-          <a
-            href="#projects"
-            onClick={() => {
-              setNavOpen(false);
-            }}
-          >
-            <li className="hover:cursor-pointer text-zorange opacity-80 mt-[20px]">
-              <FontAwesomeIcon className="h-[60px] w-[60px]" icon={faFolder} />
-              <span>Projects</span>
-            </li>
-          </a>
-          <a
-            onClick={() => {
-              setNavOpen(false);
-            }}
-            href="#contact"
-          >
-            <li className="hover:cursor-pointer text-zorange opacity-80 mt-[20px]">
-              <FontAwesomeIcon
-                className="h-[60px] w-[60px]"
-                icon={faEnvelope}
-              />
-              <span>Contact</span>
-            </li>
-          </a>
-          <div
-            onClick={() => {
-              openInNewTab("https://www.github.com/rewpt");
-              setNavOpen(false);
-            }}
-          >
-            <li className="hover:cursor-pointer text-zorange opacity-80 mt-[20px]">
-              <FontAwesomeIcon className="h-[60px] w-[60px]" icon={faGithub} />
-              <span>Github</span>
-            </li>
-          </div>
-          <div
-            onClick={() => {
-              openInNewTab(
-                "https://drive.google.com/file/d/1LvfcKpNCgrGiniY0qRUgVfzDgAr626ON/view?usp=sharing"
-              );
-              setNavOpen(false);
-            }}
-          >
-            <li className="hover:cursor-pointer text-zorange opacity-80 mt-[20px]">
-              <FontAwesomeIcon className="h-[60px] w-[60px]" icon={faFile} />
-              <span>Resume</span>
-            </li>
-          </div>
-        </ul>
-      )}
+            <div
+              onClick={() => {
+                openInNewTab(
+                  "https://drive.google.com/file/d/1LvfcKpNCgrGiniY0qRUgVfzDgAr626ON/view?usp=sharing"
+                );
+                setNavOpen(false);
+              }}
+            >
+              <li className="modal-item-container">
+                <FontAwesomeIcon className="modal-icon" icon={faFile} />
+                <span class="modal-text">Resume</span>
+              </li>
+            </div>
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </>
   );
 }
