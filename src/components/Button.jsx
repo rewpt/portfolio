e2d0006d800064b8dd7b693023530fae1ee52bd4
rewpt <zpermack@gmail.com>
2022-03-29 23:58:46 -0400

    change padding to ems

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,8 +10,8 @@ export default function Button(props) {
     <motion.button
       variants={buttonVariants}
       whileHover="hover"
-      whileTap={{ scale: 1.05, y: 2 }}
-      className="rounded text-[1.rem] py-[.4em] px-[.8em] bg-zorange text-zpurple drop-shadow-md hover:drop-shadow-xl"
+      whileTap={{ scale: 1.04, y: 2 }}
+      className="rounded text-[1.6rem] py-[.4em] px-[.8em] bg-zorange text-zpurple drop-shadow-md hover:drop-shadow-xl"
     >
       {props.children}
     </motion.button>
