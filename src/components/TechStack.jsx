4164c8112cc1d5311241bea9430603e4be6bab9c
rewpt <zpermack@gmail.com>
2022-03-29 23:58:26 -0400

    add techstack component

---
+++ src/components/TechStack.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function techStack() {
+  return (
+    <div className="stack-container">
+      <h2 className="stack-title">Tech Stack</h2>
+      <img src="" alt="" />
+    </div>
+  );
+}
