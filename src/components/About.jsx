b44a9efd82c57804bbf560c0f25896f0f9c0a81b
rewpt <zpermack@gmail.com>
2022-04-03 14:43:49 -0400

    sizing for about divs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import Lightning from "../images/about/lightning.png";
+import Agile from "../images/about/agile.png";
+import Responsive from "../images/about/responsive.svg";
+import Bolt from "../images/about/bolt.png";
 
 export default function () {
   return (
     <div className="about-container">
       {/* <h2 className="believe-title">I believe in</h2> */}
       <ul className="keyword-container">
-        <div className="relative keyword-item">
-          <img
-            className="absolute top-[-8vw] h-[15vw] max-h-[100px]"
-            src={Lightning}
-            alt="lightning bolt"
-          />
+        <div className="keyword-item">
+          <div className="h-[20vw] w-[15.4vw] max-w-[180px] max-h-[180px] mb-[1rem]">
+            <img className="" src={Bolt} alt="" />
+          </div>
           <h3 className="buzzword">Fast & scalable code</h3>
           <span className="buzz-desc">
             I build immersive apps with a seamless user experience
           </span>
         </div>
         <div className="keyword-item">
+          <div className="h-[20vw] w-[20vw] max-w-[235px] max-h-[180px] mt-[1rem] mb-[1rem]">
+            <img src={Agile} alt="" />
+          </div>
           <h3 className="buzzword">Agile Development</h3>
           <span className="buzz-desc">
             I work with customers to adjust and tweak until the product matches
             their vision
           </span>
         </div>
-        <div className="keyword-item">
+        <div className="keyword-item drop-shadow-sm">
+          <div className="h-[20vw] w-[20vw] max-w-[235px] max-h-[180px] mt-[1rem] mb-[1rem]">
+            <img src={Responsive} alt="" />
+          </div>
           <h3 className="buzzword">Responsive Layouts</h3>
           <span className="buzz-desc">
             I build apps to work across multiple platforms to use on any device
