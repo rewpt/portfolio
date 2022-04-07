52c901522c6f5b28e18db22e53a8471ef661ee45
rewpt <zpermack@gmail.com>
2022-04-06 20:48:32 -0400

    make smaller max-h and max-w

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -10,13 +10,13 @@ const logos = [PsqlLogo, ReactLogo, JsLogo, JestLogo, RubyLogo, TailwindLogo];
 
 export default function techStack() {
   return (
-    <section className="stack-container py-[3.5em] flex flex-col items-center bg-zbg">
+    <section className="stack-container">
       <h2 className="stack-title">Tech Stack</h2>
       <div className="stack-images">
         {logos.map((logo) => {
           return (
-            <div className="stack-img h-[30vw] w-[30vw] max-h-[200px] max-w-[200px]">
-              <img className="h-full w-full" src={logo} alt="psql-logo" />
+            <div className="stack-img h-[30vw] w-[30vw] max-h-[160px] max-w-[160px]">
+              <img className="h-full w-full" src={logo} alt="logo" />
             </div>
           );
         })}
