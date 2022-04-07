b41b7eb9b1c284d82e44b789cea22bcd361ef108
rewpt <zpermack@gmail.com>
2022-04-06 20:49:16 -0400

    add hook for media query for animations

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,17 @@
+export default function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addEventListener(listener);
+    return () => media.removeEventListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
