d2fc20222d377dd846ecac5a41fd4be12a0b6fae
rewpt <zpermack@gmail.com>
2022-04-09 11:43:46 -0400

    change color to green

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,7 +11,7 @@ export default function Button(props) {
       variants={buttonVariants}
       whileHover="hover"
       whileTap={{ scale: 1.04, y: 2 }}
-      className="rounded text-[1.6rem] py-[.4em] px-[.8em] bg-zorange text-zpurple drop-shadow-md hover:drop-shadow-xl"
+      className="rounded text-[1.6rem] py-[.4em] px-[.8em] bg-zdgreen text-zpurple drop-shadow-md hover:drop-shadow-xl"
     >
       {props.children}
     </motion.button>
